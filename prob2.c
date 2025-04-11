d2d77bc32c29814af3b869b35ce7552f3e701a53
Nasim <mdnasimhosen333@gmail.com>
2025-04-11 11:05:50 +0600

    solved problemm 2 - multiply

---
+++ prob2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    long long A;
+    long long B;
+    scanf("%lld %lld", &A, &B);
+    printf("%lld\n", A * B);
+    return 0;
+}
--- tempCodeRunnerFile.c
+++
@@ -1,1 +0,0 @@
-    printf("Hello, world! I am learning C programming language. ^_^ \nProgramming is fun and challenging. /\\/\\/\\\nI want to give my 100%% dedication to learn!\tI will succeed one day.\n");
